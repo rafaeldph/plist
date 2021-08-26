ed3183154f85cd769259dc464f9e56fabbc8fdcb
Rafael Perez <prafael16@gmail.com>
2021-08-26 10:14:02 -0400

    Generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  get 'dashboard/index'
+  get 'products/index'
+  get 'products/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
