a76c96b42950107e969ac60842f7871becfe7604
Rafael Perez <prafael16@gmail.com>
2021-08-28 21:38:04 -0400

    Optimize create products and variants

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,15 +2,31 @@ class Product < ApplicationRecord
   has_many :variants
 
   def self.create_many products
-    products.select {|product| self.is_valid?(product)}.each do |object|
-      product = Product.create(name: object['name'], description: object['description'])
-      Variant.create_many(product.id, object['variants'])
-    end
+    valid_products = products.select {|product| self.is_valid?(product)}.map {|product| {
+      name: product['name'],
+      description: product['description'],
+      variants: product['variants'].select {|variant| Variant.is_valid?(variant)}
+    }}
+
+    products = Product.insert_all(valid_products.map {|product| { 
+      name: product[:name], 
+      description: product[:description], 
+      created_at: DateTime.now,
+      updated_at: DateTime.now
+    }}).rows.flatten
+
+    Variant.insert_all(valid_products.each_with_index.map {|vp, i| vp[:variants].map {|variant| { 
+      name: variant['name'], 
+      price: variant['price'],
+      product_id: products[i],
+      created_at: DateTime.now,
+      updated_at: DateTime.now
+    }}}.flatten)
   end
 
   def self.is_valid? product
     !product['name'].blank? and 
       !product['description'].blank? and 
-      (product['variants'] || []).select {|variant| !variant['name'].blank? and variant['price'] > 0}.length > 0
+      (product['variants'] || []).select {|variant| Variant.is_valid?(variant)}.length > 0
   end
 end
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,12 +1,6 @@
 class Variant < ApplicationRecord
   belongs_to :product
 
-  def self.create_many product_id, variants
-    variants.select {|variant| self.is_valid?(variant)}.each do |object|
-      Variant.create(product_id: product_id, name: object['name'], price: object['price'])
-    end
-  end
-
   def self.is_valid? variant
     !variant['name'].blank? and variant['price'] > 0
   end
